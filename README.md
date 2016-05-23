da8ba7ed2a1ebdeef22cd1736eff2300ddfd714e
Rhett Lowe <rng2ml@gmail.com>
2016-05-23 13:35:17 -0700

    update readme

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Puzzle to crack a safe.
 
 
+
 ## Install 
 
 ```
@@ -16,8 +17,77 @@ $ npm install --save safe-puzzle
 var SafePuzzle = require('safe-puzzle');
 
 var safe = new SafePuzzle({
-  length: 4, // number of digits in combination
+  length: 4, // number of digits in combination; Default: 4
   maxTries: 0, // if maxTries > 0 safe will throw an 
                //    error if too many unsuccessfull attempts
+               //    Default: 0
 });
+```
+
+# Simple Puzzle
+
+Simply break the code. Nothing special about it, only four(4) digits. Just break the code 
+using whatever means you prefer.
+
+```javascript
+var SafePuzzle = require('safe-puzzle');
+
+var safe = new SafePuzzle({
+  length: 4
+});
+
+var unlocked = false;
+while (!unlocked) {
+  var tryCombo = // Generate the next combination 
+  unlocked = safe.try( tryCombo );
+}
+```
+
+
+# Running Code Puzzle
+
+The safe has a four(4) digit password. The password is entered in a 
+running fashion instead of an enter-reset fashion. ex:
+
+```javascript
+var SafePuzzle = require('safe-puzzle');
+
+var safe = new SafePuzzle({
+  length: 4
+});
+
+var unlocked = false;
+
+unlocked = safe.push('1234'); // tries '1234'
+unlocked = safe.push('5');    // tries '2345'
+unlocked = safe.push('6');    // tries '3456'
+unlocked = safe.push('7');    // tries '4567'
+unlocked = safe.push('8');    // tries '5678'
+/* etc */
+```
+
+
+# One Long String Puzzle
+
+Produce a single string which contains every possible combination, 
+includes no duplicate entries, and shorter is better.
+
+```javascript
+var SafePuzzle = require('safe-puzzle');
+
+var safe = new SafePuzzle({
+  length: 4
+});
+
+var unlocked = false;
+
+var comboString = '1234567890...'; // figure out how to make this string.
+
+safe.last = comboString.slice(0,3);
+for (var i = 3, l = comboString.length; i < l; i ++){
+  if ( safe.push( comboString[i] ) ) {
+    console.log('SUCCESS!!!', comboString.slice(i-3, i+1));
+    break;
+  }
+}
 ```
--- index.js
+++ index.js
@@ -3,43 +3,54 @@
  */
 'use strict';
 
-var extend = require('extend');
+var extend     = require('extend');
 var defOptions = {
-  length: 4,
+  length:   4,
   maxTries: 0,
 };
 
 var Safe = function (opts) {
   // Public variables
   this.options = extend({}, defOptions, opts);
-  this.tries = 0;
-  this.last = '';
+  this.tries   = 0;
+  this.last    = '';
 
   for (let i = 0, l = this.options.length; i < l; i++) {
     this.last += 0;
   }
 
   // Private variables
-  var combo = '';
+  var combo    = '';
+  var unlocked = false;
 
   // inside so it can access private vars
-  this.set = function (newCombo) {
+  this.set   = function (newCombo) {
     if (typeof newCombo !== 'string' || newCombo.length !== this.options.length) {
       throw new Error(`New combo must be length ${this.options.length}. Given: ${newCombo}`);
     }
-    combo = newCombo;
+    combo    = newCombo;
+    unlocked = false;
   };
-  this.test = function (test) {
-    this.tries ++;
-    if (this.options.maxTries && this.tries > this.options.maxTries) {
-      throw new Error('Exceeded maximum number of tries allowed');
+  this.test  = function (test) {
+    if (unlocked) {
+      throw new UnlockedError();
+    } else {
+
+      this.tries++;
+      if (this.options.maxTries && this.tries > this.options.maxTries) {
+        throw new MaxTriesError();
+      }
+
+      if (test) {
+        this.last = test;
+      }
+      unlocked = this.last === combo;
+
     }
-    if (test) {
-      this.last = test;
-    }
-    return this.last === combo;
+
+    return unlocked;
   };
-  this.try = this.test;
+  this.try   = this.test;
   this.check = this.test;
 
   // Init
@@ -55,12 +66,26 @@ Safe.prototype.randomize = function () {
 };
 
 Safe.prototype.push = function (num) {
-  return this.try(this.last.slice(1) + num);
+  return this.try((this.last + num).slice(-1 * this.options.length));
+};
+
+
+var UnlockedError = function (message) {
+  Error.call(this);
+  this.name    = 'EUNLOKD';
+  this.message = message || 'Safe already unlocked, stop trying or reset.';
+};
+var MaxTriesError = function (message) {
+  Error.call(this);
+  this.name    = 'EMAXTRIES';
+  this.message = message || 'Exceeded maximum number of unsuccessful tries.';
 };
 
 
 
-module.exports = Safe;
+module.exports               = Safe;
+module.exports.UnlockedError = UnlockedError;
+module.exports.MaxTriesError = MaxTriesError;
 
 
 var s = new Safe();
